315692707849a2f73c6e84c0bf482e8a20182a03
Kevin Shi <kshi@Kevins-MacBook-Pro.local>
2016-01-23 22:04:14 -0500

    fixed bug

--- combineTwoViews.m
+++ combineTwoViews.m
@@ -1,7 +1,17 @@
 function [ptcloud] = combineTwoViews(p1,c1,p2,c2)
+Points = p1;
+Colors = c1;
+processPointCloud;
+p1 = Points;
+c1 = Colors;
+
+Points = p2;
+Colors = c2;
+processPointCloud;
+p2 = Points;
+c2 = Colors;
 
 [Points,Colors] = affineRegistration(p1,p2,c1,c2);
-processPointCloud;
 ptcloud = triangleInterpolate(Points,Colors);
 pcshow(ptcloud);
 end

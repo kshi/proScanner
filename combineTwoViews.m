b29dd8ff2649eee5b1cb1e1d9044083699d6b1af
Kevin Shi <kshi@Kevins-MacBook-Pro.local>
2016-01-23 22:08:41 -0500

    refactored code

--- combineTwoViews.m
+++ combineTwoViews.m
@@ -1,16 +1,6 @@
 function [ptcloud] = combineTwoViews(p1,c1,p2,c2)
-Points = p1;
-Colors = c1;
-processPointCloud;
-p1 = Points;
-c1 = Colors;
-
-Points = p2;
-Colors = c2;
-processPointCloud;
-p2 = Points;
-c2 = Colors;
-
+[p1,c1] = processPointCloud(p1,c1);
+[p2,c2] = processPointCloud(p2,c2);
 [Points,Colors] = affineRegistration(p1,p2,c1,c2);
 ptcloud = triangleInterpolate(Points,Colors);
 pcshow(ptcloud);
--- processPointCloud.m
+++ processPointCloud.m
@@ -1,17 +1,20 @@
-%function [ points_out,colors_out ] = processPointCloud( points_in,colors_in )
+function [ points_out,colors_out ] = processPointCloud( points_in,colors_in )
 % removes non-face points in the point cloud
-pz = Points(:,1);
-px = Points(:,2);
-py = Points(:,3);
+pz = points_in(:,1);
+px = points_in(:,2);
+py = points_in(:,3);
 remove1 = find(pz < 0.5 | pz > 2 | px < -1.5 | px > 1.5 | py < -1.5 | py > 1.5 | isnan(pz));
-Points(remove1,:) = [];
-Colors(remove1,:) = [];
-[idx,C] = kmeans(Points,3);
+points_out = points_in;
+colors_out = colors_in;
+points_out(remove1,:) = [];
+colors_out(remove1,:) = [];
+[idx,C] = kmeans(points_out,3);
 faceInd = mode(idx);
-pz = Points(:,1);
-px = Points(:,2);
-py = Points(:,3);
+pz = points_out(:,1);
+px = points_out(:,2);
+py = points_out(:,3);
 remove2 = find(abs(C(faceInd,1) - pz) > 0.15 | abs(C(faceInd,2) - px) > 0.15 | abs(C(faceInd,3) - py) > 0.15);
-Points(remove2,:) = [];
-Colors(remove2,:) = [];
+points_out(remove2,:) = [];
+colors_out(remove2,:) = [];
+end
 
